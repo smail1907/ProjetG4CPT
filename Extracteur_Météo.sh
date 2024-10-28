d953f2d85062db168e1cf5bc8873797fa13bb2b9
abdelkerim <sabdelkerim61@gmail.com>
2024-10-28 17:39:27 +0100

    inclusion de la variante1

--- Extracteur_Météo.sh
+++ Extracteur_Météo.sh
@@ -3,11 +3,18 @@
 echo "Entrez une ville:"
 read ville
 #Récuperer les données météo de la ville rentrée par l'utilisateur
-meteo=$(curl -s "wttr.in/$ville?format=%t")
+meteo=$(curl -s "wttr.in/$ville?format=%t+%w+%h+%v")
 #Récuperation  des previsions du lendemain
 prevision_demain=$(curl -s "wttr.in/$ville?format=%t&tomorrow")
+# Récuperation de la vitesse du vent
+VENT=$(echo "$meteo" | awk '{print $2}')
+#Récuperation du  taux de l'humidité
+HUMIDITE=$(echo "$meteo" | awk '{print $3}')
+#Récuperation de la visibilité
+VISIBILITE=$(echo "$meteo" | awk '{print $4}')
+
 #date et l'heure actuelles
 date=$(date '+%Y-%m-%d')
 heure=$(date '+%H:%M')
-echo "$date - $heure - $ville : $meteo - $prevision_demain">>meteo.txt
+echo "$date - $heure - $ville : $meteo - $prevision_demain - vent :$VENT - humidité : $HUMIDITE - visibilité: $VISIBILITE">>meteo.txt
 
